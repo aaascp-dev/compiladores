b1f4dd8e489839c95b95ac64fc351435bf6a6d43
Andre Alex Araujo <andrealexaraujo@gmail.com>
2016-09-20 22:19:31 -0300

    Deletando estrutura antiga

--- tests/v0.2/expected/3.ex
+++
@@ -1,6 +0,0 @@
-float var: abba, ikea
-= abba 12.0
-= ikea + * 13.21 1.7 4.2
-bool var: svenska
-= svenska <= abba ikea
-
--- tests/v1.0/expected/2.ex
+++
@@ -1,6 +0,0 @@
-int var: i, j = 2
-int ref var: ip, jp
-= ip [addr] i
-= jp [addr] j
-= [ref] ip [ref] jp
-
